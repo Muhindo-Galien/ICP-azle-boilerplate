675684038574f95480cb0d0be6c713c7f45db5b4
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2023-08-31 14:04:58 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-// cannister code goes here
-// cannister code goes here
-import { $query, $update, Record, StableBTreeMap, Vec, match, Result, nat64, ic, Opt } from 'azle';
+import {
+  $query,
+  $update,
+  Record,
+  StableBTreeMap,
+  Vec,
+  match,
+  Result,
+  nat64,
+  ic,
+  Opt
+} from 'azle';
 import { v4 as uuidv4 } from 'uuid';
 
 type Ticket = Record<{
@@ -17,66 +26,68 @@ type TicketPayload = Record<{
     placement: nat64;
 }>
 
-const messageStorage = new StableBTreeMap<string, Ticket>(0, 44, 1024);
+const ticketStorage = new StableBTreeMap<string, Ticket>(0, 44, 1024);
 
 $query;
 export function getTickets(): Result<Vec<Ticket>, string> {
-    return Result.Ok(messageStorage.values());
+    return Result.Ok(ticketStorage.values());
 }
 
 $query;
 export function getTicket(id: string): Result<Ticket, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => Result.Ok<Ticket, string>(message),
-        None: () => Result.Err<Ticket, string>(`ticket with id=${id} not found`)
+    return match(ticketStorage.get(id), {
+        Some: (ticket) => Result.Ok<Ticket, string>(ticket),
+        None: () => Result.Err<Ticket, string>(`Ticket with id=${id} not found`)
     });
 }
 
 $update;
 export function addTicket(payload: TicketPayload): Result<Ticket, string> {
+    if (payload.placement <= 0) {
+        return Result.Err<Ticket, string>('Invalid placement value.');
+    }
+    
     const ticket: Ticket = { id: uuidv4(), createdAt: ic.time(), updatedAt: Opt.None, reserved: false, ...payload };
-    messageStorage.insert(ticket.id, ticket);
+    ticketStorage.insert(ticket.id, ticket);
     return Result.Ok(ticket);
 }
 
 $update;
 export function buyTicket(id: string): Result<Ticket, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => {
-            const ticketToBuy: Ticket = { ...message };
-            if (ticketToBuy.reserved)
-                return Result.Err<Ticket, string>(`Ticket for placement ${ticketToBuy.placement} already reserved`)
-            else {
-                const reservedTicket: Ticket = { ...message, reserved: true, updatedAt: Opt.Some(ic.time()) };
-                messageStorage.insert(message.id, reservedTicket);
+    return match(ticketStorage.get(id), {
+        Some: (ticket) => {
+            if (ticket.reserved) {
+                return Result.Err<Ticket, string>(`Ticket for placement ${ticket.placement} already reserved`);
+            } else {
+                const reservedTicket: Ticket = { ...ticket, reserved: true, updatedAt: Opt.Some(ic.time()) };
+                ticketStorage.insert(ticket.id, reservedTicket);
                 return Result.Ok<Ticket, string>(reservedTicket);
             }
         },
-        None: () => Result.Err<Ticket, string>(`ticket with id=${id} does not exist.`)
+        None: () => Result.Err<Ticket, string>(`Ticket with id=${id} does not exist.`)
     });
 }
 
 $update;
 export function revokeTicket(id: string): Result<Ticket, string> {
-    return match(messageStorage.get(id), {
-        Some: (message) => {
-            const ticketToRevoke: Ticket = { ...message }
-            if (!ticketToRevoke.reserved)
-                return Result.Err<Ticket, string>(`Ticket with placement ${ticketToRevoke.placement} is free.`)
-            else {
-                const revokedTicket: Ticket = { ...message, reserved: false };
-                return Result.Ok<Ticket, string>(revokedTicket)
+    return match(ticketStorage.get(id), {
+        Some: (ticket) => {
+            if (!ticket.reserved) {
+                return Result.Err<Ticket, string>(`Ticket with placement ${ticket.placement} is free.`);
+            } else {
+                const revokedTicket: Ticket = { ...ticket, reserved: false };
+                return Result.Ok<Ticket, string>(revokedTicket);
             }
         },
-        None: () => Result.Err<Ticket, string>(`ticket with id=${id} does not exist.`)
+        None: () => Result.Err<Ticket, string>(`Ticket with id=${id} does not exist.`)
     });
 }
 
 $update;
 export function deleteTicket(id: string): Result<Ticket, string> {
-    return match(messageStorage.remove(id), {
-        Some: (deletedMessage) => Result.Ok<Ticket, string>(deletedMessage),
-        None: () => Result.Err<Ticket, string>(`ticket with id=${id} does not exist.`)
+    return match(ticketStorage.remove(id), {
+        Some: (deletedTicket) => Result.Ok<Ticket, string>(deletedTicket),
+        None: () => Result.Err<Ticket, string>(`Ticket with id=${id} does not exist.`)
     });
 }
 
